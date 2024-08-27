046c87a63eeb51120beb7ac7a5f0bbc0c4d9b5e6
tamireshc <tamireshc@gmail.com>
2024-08-27 11:42:49 -0300

    feat: init kafka config

---
+++ kafka-producer/src/main/java/com/example/kafka_producer/config/KafkaAdminConfig.java
@@ -0,0 +1,31 @@
+package com.example.kafka_producer.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaAdminConfig {
+
+    @Autowired
+    public KafkaProperties kafkaProperties;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public KafkaAdmin.NewTopics topics() {
+        return new KafkaAdmin.NewTopics(TopicBuilder.name("str-topic").partitions(2).replicas(1).build());
+    }
+}
