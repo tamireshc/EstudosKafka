c336ed8e9ce64cbfe73f9ee3a338e8b5732e7ab3
tamireshc <tamireshc@gmail.com>
2024-08-28 11:11:39 -0300

    feat: add distinct listeners for distinc partition kafka

--- kafka-consumer/src/main/java/com/example/consumer/listeners/StrConsumerListener.java
+++ kafka-consumer/src/main/java/com/example/consumer/listeners/StrConsumerListener.java
@@ -2,14 +2,24 @@ package com.example.consumer.listeners;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(topics = "str-topic", groupId = "str-group", containerFactory = "strContainerFactory")
-    public void listen(String message) {
-        log.info("Received message: " + message);
+    @KafkaListener(topicPartitions = {
+            @TopicPartition(topic = "str-topic", partitions = {"0"})
+    }, groupId = "str-group", containerFactory = "strContainerFactory")
+    public void listener1(String message) {
+        log.info("Received message listener 1: " + message);
+    }
+
+    @KafkaListener(topicPartitions = {
+            @TopicPartition(topic = "str-topic", partitions = {"1"})
+    }, groupId = "str-group", containerFactory = "strContainerFactory")
+    public void listener2(String message) {
+        log.info("Received message listener 2: " + message);
     }
 }
