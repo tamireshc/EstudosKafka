d37cfc6e31b1bbeaf92a0fda81eadb863ca16cbd
tamires <tamireshc@gmail.com>
2024-08-28 18:42:12 -0300

    feat: add handleError2

--- kafka-consumer/src/main/java/com/example/consumer/exceptions/ErrorCustomHandler.java
+++ kafka-consumer/src/main/java/com/example/consumer/exceptions/ErrorCustomHandler.java
@@ -1,21 +1,25 @@
 package com.example.consumer.exceptions;
 
+import lombok.extern.log4j.Log4j2;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.springframework.kafka.listener.KafkaListenerErrorHandler;
 import org.springframework.kafka.listener.ListenerExecutionFailedException;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
+@Log4j2
 @Component
 public class ErrorCustomHandler implements KafkaListenerErrorHandler {
     @Override
     public Object handleError(Message<?> message, ListenerExecutionFailedException exception) {
+        log.error("Error on message: " + message.getPayload());
+        log.error("Error: " + exception.getMessage());
         return null;
     }
 
-    @Override
-    public Object handleError(Message<?> message, ListenerExecutionFailedException exception, Consumer<?, ?> consumer) {
-        return KafkaListenerErrorHandler.super.handleError(message, exception, consumer);
-    }
+//    @Override
+//    public Object handleError(Message<?> message, ListenerExecutionFailedException exception, Consumer<?, ?> consumer) {
+//        return KafkaListenerErrorHandler.super.handleError(message, exception, consumer);
+//    }
 
 }
