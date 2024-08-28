4ab9eb2c491ff5859dc29992da62edc910c852b8
tamireshc <tamireshc@gmail.com>
2024-08-28 12:05:38 -0300

    feat: add anotation customer listener

--- kafka-consumer/src/main/java/com/example/consumer/listeners/StrConsumerListener.java
+++ kafka-consumer/src/main/java/com/example/consumer/listeners/StrConsumerListener.java
@@ -1,25 +1,28 @@
 package com.example.consumer.listeners;
 
+import com.example.consumer.custom.StrCustomListener;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(topicPartitions = {
-            @TopicPartition(topic = "str-topic", partitions = {"0"})
-    }, groupId = "str-group", containerFactory = "strContainerFactory")
+    //Este listener escuta as mensagens de uma das particoes do topico str-topic
+    @StrCustomListener(groupId = "group-01")
     public void listener1(String message) {
+        log.info("Received message listener 0: " + message);
+    }
+
+    // Este listener escuta as mensagens de uma outra  particoes do topico str-topic
+    @StrCustomListener(groupId = "group-01")
+    public void listener2(String message) {
         log.info("Received message listener 1: " + message);
     }
 
-    @KafkaListener(topicPartitions = {
-            @TopicPartition(topic = "str-topic", partitions = {"1"})
-    }, groupId = "str-group", containerFactory = "strContainerFactory")
-    public void listener2(String message) {
+    // Este listener escuta as mensagens de todas as particoes do topico str-topic
+    @StrCustomListener(groupId = "group-02")
+    public void listener3(String message) {
         log.info("Received message listener 2: " + message);
     }
 }
