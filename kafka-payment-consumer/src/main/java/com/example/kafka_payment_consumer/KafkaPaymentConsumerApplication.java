cdc1439ea7e58e692093e1e23fc763f4f8eb72d8
tamires <tamireshc@gmail.com>
2024-08-29 16:22:31 -0300

    feat: create listeners for payment-consumer

---
+++ kafka-payment-consumer/src/main/java/com/example/kafka_payment_consumer/KafkaPaymentConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka_payment_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPaymentConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaPaymentConsumerApplication.class, args);
+	}
+
+}
---
+++ kafka-payment-consumer/src/main/java/com/example/kafka_payment_consumer/config/JsonConsumerConfig.java
@@ -0,0 +1,42 @@
+package com.example.kafka_payment_consumer.config;
+
+import com.example.kafka_payment_consumer.model.Payment;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.converter.BatchMessageConverter;
+import org.springframework.kafka.support.converter.BatchMessagingMessageConverter;
+import org.springframework.kafka.support.converter.JsonMessageConverter;
+
+import java.util.HashMap;
+
+@RequiredArgsConstructor
+@Configuration
+public class JsonConsumerConfig {
+
+    private final KafkaProperties properties;
+
+    @Bean
+    public ConsumerFactory<String, Object> jsonConsumerFactory() {
+        var configs = new HashMap<String, Object>();
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(configs);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Payment> jsonContainerFactory(
+            ConsumerFactory<String, Object> jsonConsumerFactory
+    ) {
+        var factory = new ConcurrentKafkaListenerContainerFactory();
+        factory.setConsumerFactory(jsonConsumerFactory);
+        return factory;
+    }
+}
